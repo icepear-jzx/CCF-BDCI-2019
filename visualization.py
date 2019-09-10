4e595ad694013249a2e97447633a2e18ed9cf03a
IcePear-Jzx <icepear.jzx@gmail.com>
2019-09-10 12:50:38 +0800

    update

--- visualization.py
+++ visualization.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
+
 import pandas as pd
+# don't show warning
+pd.set_option('mode.chained_assignment', None)
 
 
 def draw(data, ylabel=None, filter={}, ifshow=False):
-    
+
     if ylabel == None:
         # use the column named *Volume as ylabel
         for col in data.columns:
             if 'Volume' in col:
                 ylabel = col
                 break
-    
+
     # filter
+    label = ''
     for key, value in filter.items():
         data = data[data[key] == value]
+        label += str(value)
 
     # new column called regDate
     data['regDate'] = data['regYear'] * 100 + data['regMonth']
@@ -28,23 +33,24 @@ def draw(data, ylabel=None, filter={}, ifshow=False):
     # regDates are mapped to 1 ~ len(column)
     data.index = range(1, data.shape[0] + 1)
 
-    plt.plot(data[ylabel])
+    plt.plot(data[ylabel], label=label)
 
     if ifshow:
         plt.show()
 
 
-def show_salesVolume_model():
+def show_all_salesVolume(col, ifshowlabel=False):
 
     # read data
     path = 'Train/train_sales_data.csv'
     with open(path, 'r') as f:
         data = pd.read_csv(f)
-    
-    for model in set(data['model']):
-        draw(data, filter={'model': model})
-    
+
+    for item in set(data[col]):
+        draw(data.copy(), filter={col: item})
+
+    plt.title('salesVolume of all {}s'.format(col))
+    if ifshowlabel:
+        plt.legend()
     plt.show()
 
-
-show_salesVolume_model()

a0797e3e123322be18f820101b1223ccba09e623
Shichen Xu <-config>
2019-09-11 10:32:49 +0800

    add some asserts

--- dataparser.py
+++ dataparser.py
@@ -84,6 +84,8 @@ class DataParser:
                     Xi is a 2d array of feature indices of each sample in the dataset.
                     Xv is a 2d array of feature values of each sample in the dataset.
         """
+        assert not self.feat_dict == None, 'A feature dict must be generated before using this function.'
+
         dfi = pd.read_csv(self.filename)
         if self.label_name is not None:
             y = dfi[self.label_name].values
@@ -129,6 +131,9 @@ class DataParser:
         Generate itentically distributed training set and testing set,
         for the fucking competition doesn't offer a testing set.
         """
+        assert not self.feat_dict == None, 'A feature dictionary must be generated before using this function.'
+        assert not self.sparse_data == None, 'gen_vectors() must be called before using this function.'
+
         train_num = int(self.record_num * train_ratio)
         # Create a random permutation, and re-arrange the data with same order.
         permu = np.random.permutation(self.record_num)

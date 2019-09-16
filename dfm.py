9e35b40642536fb0062ca018da910f1d5ede8eec
chenlighten <xushichen@mail.ustc.edu.cn>
2019-09-16 12:38:53 +0800

    adjust some parameters

--- dfm.py
+++ dfm.py
@@ -10,10 +10,10 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 class DFM:
     def __init__(self, feature_size, field_size, 
-                 embedding_size=40, dropout_fm=[1.0, 1.0],
+                 embedding_size=80, dropout_fm=[1.0, 1.0],
                  deep_layers=[32, 32], dropout_deep=[0.5, 0.5, 0.5], 
                  deep_layers_activation=tf.nn.relu,
-                 epochs=50, batch_size=1000, 
+                 epochs=100, batch_size=1000, 
                  learning_rate=0.1,
                  use_fm=True, use_deep=True, l2_reg=0.01):
         assert use_fm or use_deep, 'At least one of use_fm and use_deep should be True.'
@@ -208,12 +208,12 @@ class DFM:
 
 
 
-def train():
+def train_random():
     dp = DataParser('Train/train_sales_data.csv', 
-        ignore_cols=['province', 'bodyType'], label_name='salesVolume', dense=False)
+        ignore_cols=['province', 'bodyType'], numeric_cols=['regYear'], label_name='salesVolume', dense=False)
     dp.gen_feat_dict()
     dp.gen_vectors()
-    Xi, Xv, Y, _, _, _ = dp.gen_train_test()
+    Xi, Xv, Y, _, _, _ = dp.gen_train_test_random()
     sep_test_data = dp.gen_fine_grained_test(partial_cols=['adcode', 'model'])
 
     dfm = DFM(dp.feat_dim, dp.field_dim)
@@ -221,9 +221,34 @@ def train():
     dfm.get_score(sep_test_data)
 
 
+def train_sequential():
+    dp = DataParser('Train/train_sequential.csv', pred_filename='Train/test_sequential.csv',
+        ignore_cols=['province', 'bodyType'], numeric_cols=['regYear'], label_name='salesVolume', dense=False)
+    dp.gen_feat_dict()
+
+    dp_train = DataParser('Train/train_sequential.csv',
+        ignore_cols=['province', 'bodyType'], numeric_cols=['regYear'], label_name='salesVolume', dense=False)
+    dp_test = DataParser('Train/test_sequential.csv',
+        ignore_cols=['province', 'bodyType'], numeric_cols=['regYear'], label_name='salesVolume', dense=False)
+    dp_train.set_feat_dict(dp.feat_dict)
+    dp_test.set_feat_dict(dp.feat_dict)
+
+    Xi_train, Xv_train, y_train = dp_train.gen_vectors()
+    Xi_test, Xv_test, y_test = dp_test.gen_vectors()
+
+    print('%d, %d'%(dp.feat_dim, dp.field_dim))
+    print('%d, %d'%(Xi_train.shape[0], Xi_train.shape[1]))
+    
+    dfm = DFM(dp.feat_dim, dp.field_dim)
+    dfm.train(Xi_train, Xv_train, y_train)
+    print(dfm.eval(Xi_test, Xv_test, y_test))
+
+    
+
+
 def predict():
     dp = DataParser('Train/train_sales_data.csv', pred_filename='Forecast/evaluation_pure.csv',
-        ignore_cols=['province', 'bodyType'], label_name='salesVolume', dense=False)
+        ignore_cols=['province', 'bodyType'], numeric_cols=['regYear'], label_name='salesVolume', dense=False)
     dp.gen_feat_dict()
     Xi, Xv, Y = dp.gen_vectors(filename='Train/train_sales_data.csv')
     Xi_eval, Xv_eval, _ = dp.gen_vectors(filename='Forecast/evaluation_pure.csv')
@@ -241,7 +266,7 @@ if __name__ == '__main__':
         print('No argument!\nArguments: "--train" for training and "--predict" for forecasting.')
         exit(0)
     if re.match('.*train', sys.argv[1]) != None:
-        train()
+        train_sequential()
     elif re.match('.*predict', sys.argv[1]) != None:
         predict()
     else:

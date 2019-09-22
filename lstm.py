18cd2db27163993a3a8cc2292e2e62f7a3298485
chenlighten <xushichen@mail.ustc.edu.cn>
2019-09-22 12:02:28 +0800

    Updata

--- lstm.py
+++ lstm.py
@@ -1,5 +1,6 @@
 import keras
 from keras import layers
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
 import os
@@ -18,6 +19,7 @@ def preprocess_train_data():
             index = (df['model'] == model) & (df['adcode'] == adcode)
             train_list.append(df[['salesVolume', 'popularity', 'carCommentVolum', 'newsReplyVolum']][index].values)
     return np.array(train_list)
+    
 
 
 def my_rmse(y_true, y_pred):
@@ -26,10 +28,11 @@ def my_rmse(y_true, y_pred):
 
 def build_lstm():
     lstm = keras.Sequential()
-    lstm.add(layers.LSTM(32, input_dim=4, return_sequences=True))
-    lstm.add(layers.LSTM(32, activation='relu'))
+    lstm.add(layers.LSTM(32, input_dim=4, return_sequences=False))
+    # lstm.add(layers.LSTM(32, activation='relu'))
+    # lstm.add(layers.Dense(32, activation='sigmoid'))
     lstm.add(layers.Dense(1))
-    lstm.compile(keras.optimizers.Adam(), loss=keras.losses.mse, metrics=[my_rmse])
+    lstm.compile(keras.optimizers.Adam(0.01), loss=keras.losses.mse, metrics=[my_rmse])
     return lstm
 
 
@@ -38,9 +41,11 @@ def main():
     print('The shape of input data is ', x.shape)
     lstm = build_lstm()
     lstm.summary()
-    for t in range(1, x.shape[1]):
-        lstm.fit(x[:, :t, :], x[:, t, 0], batch_size=32, epochs=10, validation_split=0.1, verbose=0)
-        print('rmse: %.3f'%lstm.evaluate(x[:, :t, :], x[:, t, 0])[1])
+
+    # lstm.fit(x[:, :-2, :], x[:, -2, 0], batch_size=32, epochs=40, validation_split=0.1, verbose=1)
+    for t in range(1, x.shape[1]-1):
+        lstm.fit(x[:, :t, :], x[:, t, 0], batch_size=32, epochs=5, validation_split=0.1)
+    print('rmse: %.3f'%lstm.evaluate(x[:, :-1, :], x[:, -1, 0])[1])
 
 
 if __name__ == '__main__':

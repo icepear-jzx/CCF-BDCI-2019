614acdea522e22e14dcf08ec058cfff77e8db9a7
chenlighten <xushichen@mail.ustc.edu.cn>
2019-09-25 23:41:21 +0800

    Update

---
+++ mlp4.py
@@ -0,0 +1,91 @@
+import keras
+from keras import layers
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '7'
+
+train_file = 'Train/train_extra_data.csv'
+eval_file = 'Forecast/evaluation_public.csv'
+
+
+def preprocess_train_data():
+    df = pd.read_csv(train_file)
+    train_list = []
+    for model in df['model'].unique():
+        for adcode in df['adcode'].unique():
+            index = (df['model'] == model) & (df['adcode'] == adcode)
+            train_list.append(df[['salesVolume']][index].values)
+    return np.array(train_list) # 1320 * 24 * 1
+
+
+def build_mlp():
+    input = layers.Input(shape=(4, ))
+    dense = layers.Dense(32, activation='sigmoid', kernel_initializer='he_normal')(input)
+    dense = layers.Dense(4, kernel_initializer='he_normal')(dense)
+    dense = layers.Add()([input, dense])
+    dense = layers.Activation('sigmoid')(dense)
+    dense = layers.Dense(32, activation='sigmoid', kernel_initializer='he_normal')(dense)
+    output = layers.Dense(4)(dense)
+    model = keras.Model(input, output)
+    model.compile(keras.optimizers.Adam(1e-2), loss=keras.losses.mse)
+    return model
+
+
+def my_metric(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def get_score(y_true, y_pred):
+    return 1 - np.sum(np.abs(y_pred-y_true)/y_true)/60
+
+
+def scale_fit(x):
+    assert x.shape[1] % 12 == 0
+    mu = np.zeros(shape=(12, ))
+    sigma = np.zeros(shape=(12, ))
+    for i in range(12):
+        mu[i] = np.mean(x[:, [i + 12 * j for j in range(x.shape[1]/12)]])
+        sigma[i] = np.mean(x[:, [i + 12 * j for j in range(x.shape[1]/12)]])
+    return mu, sigma
+
+
+def scale_to(x, mu, sigma, month_range):
+    assert x.shape[1] == len(month_range)
+    xs = np.zeros_like(x)
+    for i in range(x.shape[1]):
+        xs[:, i] = (x[:, i] - mu[month_range[i]])/sigma[month_range[i]]
+    return xs
+
+
+def scale_back(xs, mu, sigma, month_range):
+    assert xs.shape[1] == len(month_range)
+    x = np.zeros_like(xs)
+    for i in range(xs.shape[1]):
+        x[:, i] = xs[:, i]*sigma[month_range[i]] + mu[month_range[i]]
+    return x
+
+
+def main():
+    x = preprocess_train_data()
+    x = np.reshape(x, (-1, 24))
+    mu, sigma = scale_fit(x[:, :12])
+    x_train = np.vstack([x[:, 0:4], x[:, 4:8], x[:, 8:12]])
+    y_train = np.vstack([x[:, 12:16], x[:, 16:20], x[:, 20:24]])
+    xs_train, mu, sigma = scale(x_train)
+    ys_train = (y_train - mu)/sigma
+    print('The shape of input data is ', x.shape)
+    model = build_mlp()
+    model.summary()
+
+    model.fit(xs_train, ys_train, batch_size=64, epochs=10, validation_split=0.1, verbose=2)
+    ys_pred = model.predict(xs_train[-1320*4:-1320*3])
+    y_pred = ys_pred*sigma + mu 
+    print('rmse: %.3f'%my_metric(y_train[-1320:], y_pred))
+    # print('score: %.3f'%get_score(x[:, -1, 0], x_pred))
+
+
+if __name__ == '__main__':
+    main()

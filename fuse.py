dd5d328f9650b4c87753885f2c2eb377e734efc0
IcePear-Jzx <icepear.jzx@gmail.com>
2019-10-06 11:42:15 +0800

    update fuse

--- fuse.py
+++ fuse.py
@@ -5,6 +5,6 @@ lgb = pd.read_csv('Results/lgb-0.524.csv')
 mlp = pd.read_csv('Results/ywmlp-0.527.csv')
 tend = pd.read_csv('Results/tend-0.54.csv')
 
-fuse = 0.5 * tend + 0.5 * mlp
+fuse = 0.3 * tend + 0.7 * mlp
 
 fuse[['id','forecastVolum']].round().astype(int).to_csv('fuse.csv',encoding='utf8',index=False)

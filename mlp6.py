5cfea2c457f4ab8a2f69fbd7476d3b9cb4ada968
IcePear-Jzx <icepear.jzx@gmail.com>
2019-10-13 00:15:10 +0800

    mlp6 with exp func to deal with long tail

--- mlp6.py
+++ mlp6.py
@@ -6,6 +6,8 @@ import numpy as np
 import os
 from dataparser import write_results
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
@@ -60,21 +62,57 @@ def scale_back(xs, mu, sigma, month_range):
     return x
 
 
+def line_func(x, k, b):
+    return k * x + b
+
+
+def exp_func(x, lam, a, b):
+    return a * lam * np.exp(-lam * (x + b))
+
+
 def smooth(x):
-    mean_2016 = np.mean(x[:, :12], axis=1)
-    mean_2017 = np.mean(x[:, 12:], axis=1)
-    # (5.5, mean_2016) (17.5, mean_2017)
-    k = (mean_2017 - mean_2016) / 12
-    b = mean_2016 - k * 5.5
-    base = np.array([k * i + b for i in range(36)]).T
+    tend_x = np.arange(5.5, 18.5, 1)
+    base_x = np.arange(0, 36, 1)
+
+    tend_y = []
+    for i in range(13):
+        tend_y.append(np.mean(x[:, i:12+i], axis=1))
+    tend_y = np.array(tend_y).T
+
+    base = np.zeros(shape=(1320, 36))
+    base_revise = np.zeros(shape=(1320, 36))
+    for i in range(0,1320):
+        # para, _ = curve_fit(line_func, tend_x, tend_y[i], p0=[1, 100])
+        # k = para[0]
+        # b = para[1]
+        k = (tend_y[i][12] - tend_y[i][0]) / 12
+        b = tend_y[i][0] - k * 5.5
+        print(k,b)
+        base_y = line_func(base_x, k, b)
+        base[i] = base_y[:]
+        if k < 0:
+            para, _ = curve_fit(exp_func, tend_x, tend_y[i], p0=[1, 10000, 0], maxfev = 1000000)
+            lam = para[0]
+            a = para[1]
+            b = para[2]
+            base_y = exp_func(base_x, lam, a, b)
+        base_revise[i] = base_y[:]
+        
+        print('fit:', i)
+        
+        # plt.plot(range(24), x[i])
+        # plt.plot(tend_x, tend_y[i])
+        # plt.plot(base_x, base_y)
+        # plt.show()
+    
     xs = x - base[:, :24]
-    return xs, base
+    return xs, base, base_revise
 
 
 def main():
     x = preprocess_train_data()
     x = np.reshape(x, (-1, 24))
-    x, base = smooth(x)
+    x, base, base_revise = smooth(x)
     
     mu, sigma = scale_fit(x)
     xs_train = scale_to(x[:, :12], mu, sigma, range(0, 12))
@@ -107,12 +145,34 @@ def main():
         xs_eval = scale_to(x[:, 12:], mu, sigma, range(0, 12))
         ys_eval = model.predict(xs_eval)
         y_eval = scale_back(ys_eval, mu, sigma, range(0, 12))
+        # x = x + base[:, :24]
         y_eval = y_eval + base[:, 24:]
+
+        # deal with long tail
         for j in range(1320):
+            print('zoom:', j)
+            # top = base[j][12] # > bottom bottom_revise
             top = x[j][23] + base[j][23]
             bottom = np.min(y_eval[j][:4])
+            bottom_revise = base_revise[j][24:28] # >= 0
+            print(y_eval[j][:4])
             if bottom < 0:
-                y_eval[j][:4] = top - (top - y_eval[j][:4]) * top / (top - bottom)
+                # y_eval[j][:4] = top - (top - y_eval[j][:4]) * top / (top - bottom)
+                if (base_revise[j][24:28] == base[j][24:28]).all(): # k > 0
+                    y_eval[j][:4] = top - (top - y_eval[j][:4]) * (top - 1) / (top - bottom)
+                else: # k < 0
+                    y_eval[j][:4] = top - (top - y_eval[j][:4]) * (top - bottom_revise) / (top - bottom)
+
+            # if y_eval[j][:4].min() < 0:
+            #     print(top)
+            #     print(y_eval[j][:4])
+            #     print(base_revise[j][24:28])
+            #     print(base[j][24:28])
+            #     input()
+
+        if y_eval[:, :4].min() < 0:
+            print('Have negative number!')
+
         y_result = np.reshape(y_eval[:, :4], (1320*4), order='F')
         write_results('Results/mlp6-No.{}'.format(i), y_result)
 

b05b3cd4c08a89e6147a65c31b56fccd50a07f5e
IcePear-Jzx <icepear.jzx@gmail.com>
2019-10-12 15:14:04 +0800

    update tend2 code and result

--- tend2.py
+++ tend2.py
@@ -2,16 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import math
 from dataparser import write_results
 
 
 def normal_func(x, a, u, sig):
-    return a*np.exp(-(x - u) ** 2 / (2 * sig ** 2)) / (sig * math.sqrt(2 * math.pi))
+    return a*np.exp(-(x - u) ** 2 / (2 * sig ** 2)) / (sig * np.sqrt(2 * np.pi))
 
 
 def line_func(x, k, b):
     return k * x + b
+
+
+def exp_func(x, lam, a, b):
+    return a * lam * np.exp(-lam * (x + b))
 
 
 train_file = 'Train/train_extra_data.csv'
@@ -30,6 +33,7 @@ y = np.reshape(y, (-1, 24))
 
 # fit
 tend_x = np.arange(5.5, 18.5, 1)
+# tend_x = np.array([5.5, 11.5, 17.5])
 base_x = np.arange(0, 36, 1)
 
 tend_y = []
@@ -46,22 +50,31 @@ for i in range(0,1320):
     k = para[0]
     b = para[1]
     base_y = line_func(base_x, k, b)
-    if k < 0:
-        para, _ = curve_fit(normal_func, tend_x, tend_y[i], p0=[10000, -10, 16], maxfev = 1000000)
-        a = para[0]
-        u = para[1]
-        sig = para[2]
-        base_y = normal_func(base_x, a, u, sig)
+    show = False
+    if base_y.min() < 0:
+        # para, _ = curve_fit(normal_func, tend_x, tend_y[i], p0=[10000, -10, 16], maxfev = 1000000)
+        # a = para[0]
+        # u = para[1]
+        # sig = para[2]
+        # base_y = normal_func(base_x, a, u, sig)
+        para, _ = curve_fit(exp_func, tend_x, tend_y[i], p0=[1, 10000, 0], maxfev = 1000000)
+        print(para)
+        lam = para[0]
+        a = para[1]
+        b = para[2]
+        base_y = exp_func(base_x, lam, a, b)
+        # show = True
     
     for j in range(12):
         y[i][24+j] = (0.25*y[i][11+j] + 0.5*y[i][12+j] + 0.25*y[i][13+j]) * base_y[24+j] / base_y[12+j]
         # y[i][24+j] = (y[i][12+j]) * base_y[24+j] / base_y[12+j]
 
-    # plt.axvline(23)
-    # plt.plot(range(36), y[i])
-    # plt.plot(tend_x, tend_y[i])
-    # plt.plot(base_x, base_y)
-    # plt.show()
+    if show:
+        plt.axvline(23)
+        plt.plot(range(36), y[i])
+        plt.plot(tend_x, tend_y[i])
+        plt.plot(base_x, base_y)
+        plt.show()
 
 y_result = np.reshape(y[:, 24:28], (1320*4), order='F')
 write_results('Results/tend2', y_result)
